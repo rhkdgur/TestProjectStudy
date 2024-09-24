1be11d04f7bae921a6121fd5c5cbed1f61e9171f
고광혁 <rhkdgur195@gmail.com>
2024-09-24 21:28:33 +0900

    [LS-3] 멀티모듈 build gradle 설정

---
+++ search-api/src/main/java/com/library/Main.java
@@ -0,0 +1,18 @@
+package com.library;
+
+/**
+ * packageName    : com.library
+ * fileName       : ${NAME}
+ * author         : rhkdg
+ * date           : 2024-09-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-24        rhkdg       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
