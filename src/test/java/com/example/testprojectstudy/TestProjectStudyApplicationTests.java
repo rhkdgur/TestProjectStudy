faa986d7e2f89b776d6af3c75245c712f146654f
고광혁 <rhkdgur195@gmail.com>
2024-09-24 16:43:25 +0900

    initial commit

---
+++ src/test/java/com/example/testprojectstudy/TestProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprojectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
